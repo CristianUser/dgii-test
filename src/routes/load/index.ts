15f343c9a562cf6677f91c5c5fe3f34e1deafc3f
Cristian Mejia <cristianmejia97@gmail.com>
2025-06-20 23:04:56 -0400

    disable load endpoint for prod

--- src/routes/load/index.ts
+++ src/routes/load/index.ts
@@ -3,8 +3,10 @@ import { loadData } from "../../utils";
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
-
-    const result = await loadData()
+    if (process.env.NODE_ENV === "production") {
+      return { status: "ok", took: "0.0ms" };
+    }
+    const result = await loadData();
 
     return { status: result.status, took: `${result.took}ms` };
   });
